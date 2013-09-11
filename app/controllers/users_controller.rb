2d31da25a9a9300a20d951bd17ef5c82ef7d5533
inframatic <isteffy@hotmail.com>
2013-09-11 12:25:33 -0400

    Creating log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,16 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
+  	  	
   	  	if @user.save
   		redirect_to products_url, :notice => "Signed up!"
-  	else
+  		else
   		render "new"
   	end
   end
 
   private
   def user_params
-  	params.require(:user).permit(:email, :password_digest)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
